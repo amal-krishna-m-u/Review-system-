9c04a6963ffff7572577afb3b50feb0d074c09e4
Amal krishna M.U <83550433+amal-krishna-m-u@users.noreply.github.com>
2023-01-03 10:10:19 +0530

    Create session.py

---
+++ session.py
@@ -0,0 +1,17 @@
+<?php
+   include('config.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($conn,"select * from User_details where user_name = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+  
+    $_SESSION['login_session'] = $row['user_id'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:index.php");
+      die();
+   }
+?>
